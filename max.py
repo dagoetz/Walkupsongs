f599f1eabdb48f99084312eb81febd7361e89f9c
Daniel Goetz <dagoetz@syr.edu>
2018-11-29 18:38:33 -0500

    spotipy

---
+++ max.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+import os
+import sys
+import json
+import spotipy
+import webbrowser
+import spotipy.util as util
+from json.decoder import JSONDecodeError
+
+username=sys.argv[1]
+
+# USerID: crfv73qfpinw7aq923mh3h6jv
+
+try:
+    token=util.prompt_for_user_token(username)
+    
+except:
+    os.remove(f".cache-{username}")
+    token=util.prompt_for_user_token(username)
+
+Object=spotipy.spotify(auth=token)
+    
+
